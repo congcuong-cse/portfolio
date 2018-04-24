e7b4adbdafe06681541929e667d292f1e40926a1
Công Cương Phạm <congcuong.cse@gmail.com>
2018-04-25 00:13:49 +0700

    Deploy to gh-pages

--- src/portfolio/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: 'babel-eslint',
-  extends: 'airbnb',
-  rules: {
-    'react/prefer-stateless-function': 'off',
-    'react/jsx-filename-extension': 'off,',
-    'react/react-in-jsx-scope': 'off,',
-  },
-};
--- src/portfolio/env-config.js
+++
@@ -1,5 +0,0 @@
-const prod = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  'process.env.ASSET_PREFIX': prod ? '.' : '',
-};
